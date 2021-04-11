47c9c83a474c097039a41e87608d025ed50abf51
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2021-04-10 21:07:55 -0600

    Add files via upload
    added functionality to buttons, opening one articles text will close any other open text

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-//using selectors inside the element
-// traversing the dom
-const btns = document.querySelectorAll('.question-btn');
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        const question = e.currentTarget.parentElement.parentElement;
+// using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    // console.log(question);
+    const btn = question.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener('click', function() {
+        // item is the same as question
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+
         question.classList.toggle('show-text');
     });
 });
+
+// traversing the dom
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
